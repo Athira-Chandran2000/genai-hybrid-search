768cdddb88873c7e6f96e3fcc2ca68e89c387caf
Athira Chandran <athirachandran4224@gmail.com>
2026-01-21 12:12:30 +0530

    Fix chroma tentent issue by using in-meomry vectorstore

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -1,18 +1,20 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from chromadb.config import Settings
 
 
 def create_vectorstore(documents):
-    """
-    Create a Chroma vector store from documents.
-    """
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
     vectorstore = Chroma.from_documents(
         documents=documents,
-        embedding=embeddings
+        embedding=embeddings,
+        client_settings=Settings(
+            persist_directory=None,   # ⬅️ IMPORTANT
+            anonymized_telemetry=False
+        )
     )
 
     return vectorstore
